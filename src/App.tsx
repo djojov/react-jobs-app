1cb189299686566a9bf5bdaf324f6ce261872ef1
Djordje Jovanovic <djordje.jovanoviic@gmail.com>
2024-03-21 15:00:30 +0100

    Add routes for JobsPage and update links

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<MainLayout />}>
 			<Route index element={<HomePage />} />
+			<Route path="jobs" element={<JobsPage />} />
 		</Route>
 	)
 );
