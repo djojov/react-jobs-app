9e2bd375865551c4545b61b14d8ce363c6bf6c08
Djordje Jovanovic <djordje.jovanoviic@gmail.com>
2024-03-21 16:30:36 +0100

    Add JobPage component and update JobListing link

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
+import JobPage, { jobLoader } from './pages/JobPage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 
@@ -9,6 +10,7 @@ const router = createBrowserRouter(
 		<Route path="/" element={<MainLayout />}>
 			<Route index element={<HomePage />} />
 			<Route path="jobs" element={<JobsPage />} />
+			<Route path="jobs/:id" element={<JobPage />} loader={jobLoader} />
 			<Route path="*" element={<NotFoundPage />} />
 		</Route>
 	)
--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -57,7 +57,7 @@ export default function JobListing(props: JobListingProps) {
 						{props.job.location}
 					</div>
 					<Link
-						to={`/job/${props.job.id}`}
+						to={`/jobs/${props.job.id}`}
 						className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
 					>
 						Read More
