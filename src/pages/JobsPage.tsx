e17e8aeaaa924372233470da77e688e354bc63d4
Djordje Jovanovic <djordje.jovanoviic@gmail.com>
2024-03-21 15:16:36 +0100

    Refactor JobListings component and add isHome prop

--- src/pages/JobsPage.tsx
+++ src/pages/JobsPage.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import JobListings from '../components/JobListings';
 
 export default function JobsPage() {
-	return <div>JobsPage</div>;
+	return (
+		<section className="bg-blue-50 px-4 py-6">
+			<JobListings />
+		</section>
+	);
 }
