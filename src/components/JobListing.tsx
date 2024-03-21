674ac9c120374b6e853dfdb3bb9b7b9dd99a3555
Djordje Jovanovic <djordje.jovanoviic@gmail.com>
2024-03-21 14:41:06 +0100

    Add react-icons package and provide location icon to job listing

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+
 type JobType = {
 	id: string;
 	title: string;
@@ -18,15 +21,30 @@ interface JobListingProps {
 }
 
 export default function JobListing(props: JobListingProps) {
+	const [showFullDescription, setShowFullDescription] = useState(false);
+
+	let description = props.job.description;
+
+	if (!showFullDescription) {
+		description = description.substring(0, 90) + '...';
+	}
+
 	return (
 		<div className="bg-white rounded-xl shadow-md relative">
 			<div className="p-4">
 				<div className="mb-6">
-					<div className="text-gray-600 my-2">{props.job.id}</div>
+					<div className="text-gray-600 my-2">{props.job.type}</div>
 					<h3 className="text-xl font-bold">{props.job.title}</h3>
 				</div>
 
-				<div className="mb-5">{props.job.description}</div>
+				<div className="mb-5">{description}</div>
+
+				<button
+					className="text-indigo-500 mb-5 hover:text-indigo-600"
+					onClick={() => setShowFullDescription((prevState) => !prevState)}
+				>
+					{showFullDescription ? 'Less' : 'More'}
+				</button>
 
 				<h3 className="text-indigo-500 mb-2">{props.job.salary}</h3>
 
@@ -34,7 +52,7 @@ export default function JobListing(props: JobListingProps) {
 
 				<div className="flex flex-col lg:flex-row justify-between mb-4">
 					<div className="text-orange-700 mb-3">
-						<i className="fa-solid fa-location-dot text-lg"></i>
+						<FaMapMarkerAlt className="inline text-lg mb-1 mr-1" />
 						{props.job.location}
 					</div>
 					<a
