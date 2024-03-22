9f7df707b690a85f03aa98363754aeff3594a925
Djordje Jovanovic <djordje.jovanoviic@gmail.com>
2024-03-22 17:15:41 +0100

    Update job page and add edit job functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import { JobType } from './components/JobListing';
 import MainLayout from './layouts/MainLayout';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 import HomePage from './pages/HomePage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import JobsPage from './pages/JobsPage';
@@ -26,6 +27,17 @@ function App() {
 		return;
 	};
 
+	const updateJob = async (updatedJob: JobType) => {
+		const res = await fetch(`/api/jobs/${updatedJob.id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(updatedJob),
+		});
+		return;
+	};
+
 	const router = createBrowserRouter(
 		createRoutesFromElements(
 			<Route path="/" element={<MainLayout />}>
@@ -33,6 +45,7 @@ function App() {
 				<Route path="jobs" element={<JobsPage />} />
 				<Route path="add-job" element={<AddJobPage addJobSubmit={addJob} />} />
 				<Route path="jobs/:id" element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+				<Route path="/edit-job/:id" element={<EditJobPage updateJobSubmit={updateJob} />} loader={jobLoader} />
 				<Route path="*" element={<NotFoundPage />} />
 			</Route>
 		)
